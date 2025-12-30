6ef9874f44d7a23f79a16a0c24d576db1431c9c5
Denis <deniskatarasenko6@gmail.com>
2025-12-30 15:47:03 +0100

    refactor: remove commented-out table code from CollectionsPage component

--- app/(admin)/admin/collections/page.tsx
+++ app/(admin)/admin/collections/page.tsx
@@ -14,38 +14,6 @@ export default async function CollectionsPage() {
       <PageHeader>Collections</PageHeader>
 
       <DataTable columns={columns} data={collections} />
-
-      {/* <Table className="mt-10">
-        <TableHeader>
-          <TableRow>
-            <TableHead>ID</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Created at</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {collections.map((c) => (
-            <TableRow>
-              <TableCell>{c.id}</TableCell>
-              <TableCell>{c.name}</TableCell>
-              <TableCell>{c.description?.slice(0, 30)}...</TableCell>
-              <TableCell>{formatDateForAdminTables(c.createdAt)}</TableCell>
-              <TableCell>
-                <Link href={`/admin/collections/${c.id}/edit`}>Edit</Link>
-              </TableCell>
-            </TableRow>
-          ))}
-
-          {collections.length === 0 && (
-            <TableRow>
-              <TableCell colSpan={4}>No collections yet.</TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table> */}
     </PageContainer>
   );
 }
